feab8dfc6ed06b79abaf1b97fc0c9f4f2fb4f922
Greatenishioka <20e1118@andrew.ac.jp>
2026-02-22 02:09:44 +0900

    fix DDDわからん。頑張る

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,39 +1,42 @@
+// Video関係のLambda関数
+
 mod domain;
 mod infrastructure;
 
+use crate::domain::video::entity::video_entity::VideoId;
+use crate::domain::video::usecase::get_video::GetVideoUseCase;
+use crate::infrastructure::in_memory_repo::InMemoryRepo;
 use axum::{extract::Extension, routing::get, Router};
 use lambda_http::{run, Error};
 use serde_json;
 use std::sync::Arc;
-use crate::infrastructure::in_memory_repo::InMemoryRepo;
-use crate::domain::video::usecase::get_video::GetVideoUseCase;
-use crate::domain::video::entity::video_entity::VideoId;
+use tracing::info;
 
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+    info!("ここでAxum起動");
+    run(router()).await
+}
+
+// ルーター定義
 fn router() -> Router {
     // composition root: 実装をここで生成して注入する
     let repo = Arc::new(InMemoryRepo::new());
-    Router::new()
-        .route("/", get(root))
-        .route("/demo_videos", get(demo_videos))
-        .layer(Extension(repo))
+
+    let v1_api = Router::new()
+        .route("/videos", get(get_videos))
+        // ここもうちょっとマシにしたい
+        .layer(Extension(repo));
+
+    return Router::new().nest("/api", Router::new().nest("/v1", v1_api));
 }
 
-pub async fn root() -> &'static str {
-    "Hello Axum!"
-}
-
-async fn demo_videos(
-    Extension(repo): Extension<Arc<InMemoryRepo>>,
-) -> String {
+async fn get_videos(Extension(repo): Extension<Arc<InMemoryRepo>>) -> String {
     let uc = GetVideoUseCase::new(repo.clone());
     let id = VideoId::new();
     match uc.execute(&id).await {
         Ok(v) => serde_json::to_string(&v).unwrap_or_default(),
         Err(_) => "[]".to_string(),
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    run(router()).await
 }
